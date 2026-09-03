432ff0f7ae1fd94b41fdae760288606d7b4e5aa2
Qifaw150922 <qifawtechnologies@gmail.com>
2026-09-03 11:38:08 +0530

    Refactor WSGI application initialization
    Refactor WSGI application setup to use create_app function and load environment.

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,12 @@
-from app import app
+# wsgi.py
+import os
+from app import create_app
 
-application = app
+# Load environment
+env = os.environ.get('FLASK_ENV', 'production')
 
-if __name__ == '__main__':
-    application.run()
+# Create the application instance
+application = create_app(env)
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=5000)
