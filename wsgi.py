481f2ff7e0c01b689085411dafb7f73ae42c2f61
Qifaw150922 <qifawtechnologies@gmail.com>
2026-09-04 09:00:05 +0530

    Refactor import statement for application

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,1 @@
-import os
-import sys
-
-# Ensure Python can find modules inside /backend
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
-
-from wsgi import application
-
-if __name__ == "__main__":
-    application.run()
+from application import application
